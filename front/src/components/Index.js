393a2298d16ba37887a44f0cb437f8ce6c7e4853
Roberto <rob.sosa13@gmail.com>
2021-10-05 17:26:33 -0400

    teste-pil

--- front/src/components/Index.js
+++ front/src/components/Index.js
@@ -50,19 +50,7 @@ class Index extends Component {
             return (
                 <>
                     <Container>
-                        <Alert variant="success">
-                            <Alert.Heading>Hey, nice to see you</Alert.Heading>
-                            <p>
-                                Aww yeah, you successfully read this important alert message. This example
-                                text is going to run a bit longer so that you can see how spacing within an
-                                alert works with this kind of content.
-                            </p>
-                            <hr />
-                            <p className="mb-0">
-                                Whenever you need to, be sure to use margin utilities to keep things nice
-                                and tidy.
-                            </p>
-                        </Alert>
+                  
                         <h1>Lista de productos</h1>
 
                         <input class="form-control"
@@ -93,11 +81,10 @@ class Index extends Component {
                                             <td> <img src={item.product_image}/> </td>
 
 
-                                            <button className="btn btn-primary"  > Test
-                                            </button>
+                                    
                                             <td>
                                                 <button className="btn btn-success">
-                                                    Boleta</button>
+                                                    Ver</button>
                                             </td>
                                         </tr>
                                     ))
